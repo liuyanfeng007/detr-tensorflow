894cc1afaaefed1627c43d731a06cc8cc2de3c4e
liuyanfeng <34180204+liuyanfeng007@users.noreply.github.com>
2024-12-29 22:08:37 +0900

    Update optimizers.py
    AttributeError: 'Adam' object has no attribute '_serialize_hyperparameter'

--- detr_tf/optimizers.py
+++ detr_tf/optimizers.py
@@ -83,10 +83,15 @@ def setup_optimizers(model, config):
     disable_batchnorm_training(model)
 
     # Optimizers
-    backbone_optimizer = tf.keras.optimizers.Adam(learning_rate=get_backbone_learning_rate, clipnorm=config.gradient_norm_clipping)
-    transformers_optimizer = tf.keras.optimizers.Adam(learning_rate=get_transformers_learning_rate, clipnorm=config.gradient_norm_clipping)
-    nlayers_optimizer = tf.keras.optimizers.Adam(learning_rate=get_nlayers_learning_rate, clipnorm=config.gradient_norm_clipping)
+    #backbone_optimizer = tf.keras.optimizers.Adam(learning_rate=get_backbone_learning_rate, clipnorm=config.gradient_norm_clipping)
+    #transformers_optimizer = tf.keras.optimizers.Adam(learning_rate=get_transformers_learning_rate, clipnorm=config.gradient_norm_clipping)
+    #nlayers_optimizer = tf.keras.optimizers.Adam(learning_rate=get_nlayers_learning_rate, clipnorm=config.gradient_norm_clipping)
+    backbone_optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=get_backbone_learning_rate, clipnorm=config.gradient_norm_clipping)
+    transformers_optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=get_transformers_learning_rate, clipnorm=config.gradient_norm_clipping)
+    nlayers_optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=get_nlayers_learning_rate, clipnorm=config.gradient_norm_clipping)
 
+    tf.keras.optimizers.legacy.Adam
+    
     # Set trainable variables
 
     backbone_variables, transformers_variables, nlayers_variables = [], [], []
